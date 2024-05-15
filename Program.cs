708451a7ce7961b37e18bdcf360c10bd0d8c12ba
weaklogics <128999210+weaklogics@users.noreply.github.com>
2024-05-15 16:07:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Assignment.Model;
+using Assignment.Repository;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            IstudentRepository studentRepository = new StudentRepository();
+            List<Student> allStudents = studentRepository.DisplayStudentsInfo();
+            foreach (Student item in allStudents)
+            {
+                Console.WriteLine(item);
+            }
+            Student student = new Student();
+            
+
+           List<Enrollment> enrollments = studentRepository.GetEnrolledCourses(student);
+           foreach (Enrollment enrollment in enrollments)
+            {
+                Console.WriteLine(enrollment);
+            }
+
+
+
+        }
+    }
+}
