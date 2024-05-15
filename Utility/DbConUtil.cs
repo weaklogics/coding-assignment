b781d19ecdd014ee1823cabba875c11b888f0646
weaklogics <128999210+weaklogics@users.noreply.github.com>
2024-05-15 16:07:24 +0530

    Add files via upload

---
+++ Utility/DbConUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Assignment.Utility
+{
+   
+        //Microsoft.Extensions.Configuration
+        //Microsoft.Extensions.Configuration.FileExtension
+        //Microst.extensions.Configuration.json
+        internal static class DbConUtil
+        {
+            private static IConfiguration _iconfiguration;
+            //create a constructor
+            static DbConUtil()
+            {
+                GetAppSettingsFile();
+            }
+
+            private static void GetAppSettingsFile()
+            {
+                var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json");
+                _iconfiguration = builder.Build();
+
+            }
+
+            public static string GetConnectionString()
+            {
+                return _iconfiguration.GetConnectionString("LocalConnectionString");
+            }
+        }
+    }
+
